ffe52283788de048608a3c9132b67c2db80ac538
Diego Scataglini <diego@junivi.com>
2011-08-12 11:28:45 -0400

    trialing compojure hiccup ring setup

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject BioClojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"][commons-lang "2.3"] ])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [commons-lang "2.3"]
+                 [compojure "0.6.4"]
+                 [hiccup "0.3.6"]]
+  :dev-dependencies [[lein-ring "0.4.5"]]
+  :ring {:handler BioClojure.web.core/app})
